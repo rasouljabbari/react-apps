becd0fe77eddea89ac064931e4d80fcf9e5891f3
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2023-09-03 12:40:30 +0330

    error boundary

--- src/components/react-query/Todos/TodoList.jsx
+++ src/components/react-query/Todos/TodoList.jsx
@@ -1,33 +1,33 @@
-import {memo} from 'react';
-import {useQuery} from "react-query";
+import { memo } from 'react';
+import { useQuery } from "react-query";
 import DeleteTodo from "./DeleteTodo";
 import AddTodo from "./AddTodo";
 
 function TodoList() {
 
-    const {data, isLoading, isError} = useQuery('todos', async () => {
+    const { data: todos, error } = useQuery('todos', async () => {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos')
         return await response.json()
     }, {
         staleTime: 10000, // 10 seconds
         cacheTime: 60000, // 1 minute
-        staleWhileRevalidate: true
+        staleWhileRevalidate: true,
+        suspense: true
     })
 
-    if (isLoading) return <div>Loading...</div>
-    if (isError) return <div>Error loading data</div>
+    console.log(error, todos);
 
     return (
         <>
-
             <ul>
                 {
-                    data.map(({id, title}) => (
-                        <li key={id}>{title} <DeleteTodo id={id}/></li>
+                    todos.map(({ id, title }) => (
+                        <li key={id}>{title} <DeleteTodo id={id} /></li>
                     ))
                 }
+                <div>{vsdvdsvdsv}</div>
             </ul>
-            <AddTodo/>
+            <AddTodo />
         </>
     );
 }
